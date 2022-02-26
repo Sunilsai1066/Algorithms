867cfc6efedf158b0fcefeea42e6a6421707cf50
Sunilsai1066 <sunilsai6268@gmail.com>
2022-02-26 22:49:38 +0530

    L3 Added

---
+++ Striver_Trie/Longest Word With All Prefixes.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self):
+        self.NodeLinks = [0]*26
+        self.End = False
+        
+def completeString(a):
+    Root = Node()
+    RtCopy = Root
+    Res = ""
+    ResMaxVal = 0
+    for word in a:
+        Root = RtCopy
+        for letter in word:
+            if(Root.NodeLinks[ord(letter)-97] == 0):
+                NewNode = Node()
+                Root.NodeLinks[ord(letter)-97] = NewNode
+                Root = Root.NodeLinks[ord(letter)-97]
+            else:
+                Root = Root.NodeLinks[ord(letter)-97]
+        Root.End = True
+    
+    for word in a:
+        Root = RtCopy
+        WordLen = len(word)
+        TrueCount = 0
+        if(len(word) >= ResMaxVal):
+            for letter in word:
+                if(Root.NodeLinks[ord(letter)-97] != 0):
+                    Root = Root.NodeLinks[ord(letter)-97]
+                    if(Root.End == True):
+                        TrueCount += 1
+                    else:
+                        break
+            if(WordLen == TrueCount and WordLen >= ResMaxVal):
+                if(Res != None and word<Res):
+                    Res = word
+                    ResMaxVal = WordLen
+                else:
+                    Res = word
+                    ResMaxVal = WordLen
+    return Res if Res else None
+
+Str = "n ni nin ninj ninja ninga"
+a = Str.split(" ")
+print(completeString(a))
